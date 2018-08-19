05c760418be36d02ce2342d25eec504123866ad2
Santiago Sendon <santiagosendon92@gmail.com>
2018-08-19 19:07:36 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,26 +2,4 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(attendees)
-  attendees.map do |name|
-    badge_maker(name)
-  end
 
-end
-
-def assign_rooms(attendees)
-  room=0
-  attendees.map do |name|
-    room+=1
-    "Hello, #{name}! You'll be assigned to room #{room}!"
-  end
-end
-
-def printer(attendees)
-  batch_badge_creator(attendees).each do |value|
-    puts value
-  end
-  assign_rooms(attendees).each do |value|
-    puts value
-  end
-end
